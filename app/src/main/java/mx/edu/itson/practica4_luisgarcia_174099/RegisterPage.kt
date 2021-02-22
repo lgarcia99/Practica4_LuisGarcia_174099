2f487186b04fbfd57847d8375c4d41fd91e90f41
David Garcia <luis_david_26g@hotmail.com>
2021-02-22 01:07:51 -0700

    Welcome and Register screens

---
+++ app/src/main/java/mx/edu/itson/practica4_luisgarcia_174099/RegisterPage.kt
@@ -0,0 +1,12 @@
+package mx.edu.itson.practica4_luisgarcia_174099
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_page)
+    }
+}
